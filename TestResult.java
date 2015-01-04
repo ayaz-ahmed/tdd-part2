b8e92ccc6fb51efedff9634ca93995e58b4f9da0
ayaz-ahmed <ayazahmed_@hotmail.com>
2015-01-04 14:11:11 +0500

    Chapter 22 (04 - Jan - 2015)

--- TestResult.java
+++ TestResult.java
@@ -2,17 +2,22 @@
 public class TestResult {
 
 	private int runCount;
+	private int errorCount;
 	
 	public TestResult(){
-		this.runCount = 0;	
+		this.runCount = 0;
+		this.errorCount = 0;
 	}
 	
 	public void testStarted(){
 		this.runCount = this.runCount + 1;
 	}
+	
+	public void testFailed(){
+		this.errorCount = this.errorCount + 1;
+	}
+	
 	public String summary(){
-		return runCount + " run, 0 failed";
+		return this.runCount + " run, " + this.errorCount + " failed";		
 	}
 }
-
-
