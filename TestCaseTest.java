30e39a30b88db7acd577903492c34228e2acdda1
ayaz-ahmed <ayazahmed_@hotmail.com>
2015-01-04 13:50:40 +0500

    Chapter 21 (04 - Jan - 2015)

--- TestCaseTest.java
+++ TestCaseTest.java
@@ -1,5 +1,6 @@
 public class TestCaseTest {
 	WasRun test;
+	TestResult result;
 	
 	public void setUp(){
 		test = new WasRun("testMethod");
@@ -11,8 +12,22 @@ public class TestCaseTest {
 		assert("setUp testMethod tearDown " == test.log);
 	}
 	
+	public void testResult() throws Exception{
+		test = new WasRun("testMethod");
+		result = test.run();
+		assert("1 run, 0 failed" == result.summary());
+	}
+	
+	public void testFailedResult() throws Exception{
+		test = new WasRun("testBrokenMethod");
+		result = test.run();
+		assert("1 run, 1 failed" == result.summary());
+	}
+	
+	
 	public static void main(String args[]) throws Exception{	
 		new TestCaseTest().setUp();
 		new TestCaseTest().testTemplateMethod();
 	}
 }
+
