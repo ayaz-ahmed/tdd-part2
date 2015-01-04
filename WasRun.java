9d8219323857e21676e36038009039530eeb85f6
ayaz-ahmed <ayazahmed_@hotmail.com>
2015-01-04 12:33:03 +0500

    Chapter 19 (04 - Jan - 2015)

--- WasRun.java
+++ WasRun.java
@@ -1,13 +1,21 @@
 public class WasRun extends TestCase{
 
 	public boolean wasRun = false;
+	public boolean wasSetUp;
 
 	public WasRun(String methodName) {
 		super(methodName);
 		this.wasRun = false;
+		this.wasSetUp = true;
 	}
 
 	public void testMethod(){
 		this.wasRun = true;
 	}
+	
+	public void setUp(){
+		this.wasRun = false;
+		this.wasSetUp = true;
+	}
+	
 }
